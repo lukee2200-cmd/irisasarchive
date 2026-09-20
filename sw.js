4f8a884ea3b3f372a6e10b6f48af9c38a3356d01
lukee2200-cmd <lukee2200@gmail.com>
2026-09-20 04:55:28 -0400

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,21 @@
+const VERSION = 'irisa-v4';
+const SHELL = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(VERSION).then(c => c.addAll(SHELL)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== VERSION).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', e => {
+  const req = e.request;
+  if (req.method !== 'GET') return;
+  const url = new URL(req.url);
+  const cacheable = url.origin === location.origin || /(^|\.)(fonts\.googleapis\.com|fonts\.gstatic\.com|cdn\.jsdelivr\.net)$/.test(url.hostname);
+  if (!cacheable) return;
+  e.respondWith(caches.open(VERSION).then(async cache => {
+    const hit = await cache.match(req, { ignoreSearch: true });
+    const net = fetch(req).then(res => { if (res && (res.ok || res.type === 'opaque')) cache.put(req, res.clone()); return res; }).catch(() => hit);
+    return hit || net;
+  }));
+});
